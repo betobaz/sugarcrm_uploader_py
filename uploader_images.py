fb402e00be2198e6e2013122e0c0cf1ad3eccaff
Betobaz <pedroalberto007@gmail.com>
2015-09-14 13:23:57 -0500

    Update uploader_images.py

--- uploader_images.py
+++ uploader_images.py
@@ -15,19 +15,17 @@ field_name = ""
 csv_import = ""
 module_name = ""
 
-
-
 api = SugarCRMAPI(host, clientId, clientSecret)
 result = api.oauth2_token(user_name, password)
 
 with open(csv_import, 'rb') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		url = "{module_name}/{1}/file/{3}?format=json&delete_if_fails=true&oauth_token={2}".format(module_name=module_name,row['producttemplate_id'], api.access_token, )
+		url = "{module_name}/{producttemplate_id}/file/{field_name}?format=json&delete_if_fails=false&oauth_token={access_token}".format(module_name=module_name,producttemplate_id=row['producttemplate_id'], access_token=api.access_token,field_name=field_name )
 		path = "{1}/{0}".format(row['path'],directory)
 		file_content = open(path,'rb')
 		filename, file_extension = os.path.splitext(path)
-		content_type = mimetypes.types_map[file_extension]
+		content_type = mimetypes.types_map[file_extension.lower()]
 		result = api.upload(url, file_content, content_type, field_name)
 		print "{0}#ProductTemplates/{1}".format(host,row['producttemplate_id'])
 
